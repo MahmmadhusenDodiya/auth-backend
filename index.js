d9dcd430fae970e1f5f495594bf6fb3e77f9f90e
MahmmadhusenDodiya <rajudodiya072@gmail.com>
2024-07-28 19:26:47 +0530

    Docker file added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import cookieParser from "cookie-parser";
 
 
 dotenv.config();
-const port=process.env.PORT || 5000;
+const port=process.env.PORT || 8081;
 
 const app=express();
 app.use(express.json());
